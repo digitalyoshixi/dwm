8d961eeaf053ef141bdee26160c5ac98dabc3e9b
digitalyoshixi <davidchenyuhe@proton.me>
2023-08-06 13:26:02 -0400

    alpha patch

--- config.h
+++ config.h
@@ -6,18 +6,25 @@ static const unsigned int gappx     = 6;        /* gap size between windows*/
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosveka:size=13" };
-static const char dmenufont[]       = "Iosevka:size=13";
+static const char *fonts[]          = { "monospace:size=13" };
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
 static const char col_seagreen[]    = "#00cc99";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_seagreen,  col_seagreen  },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border*/
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
